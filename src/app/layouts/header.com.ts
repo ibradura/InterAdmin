aee6927c25c0f86c37b335e551ff14b5b81df741
iduran <iduran@mngkargo.com.tr>
2017-10-19 15:09:14 +0300

    ilk

---
+++ src/app/layouts/header.com.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'header-layout',
+    templateUrl: 'header.com.html'
+})
+
+export class HeaderComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
